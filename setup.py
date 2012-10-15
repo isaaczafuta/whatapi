e48be813a52a7bf5fcd63bb4ecd03e531d36a821
Isaac Zafuta <isaac@zafuta.com>
2012-10-14 21:09:09 -0700

    Bump version to 0.1.0, set up for pypi

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 from setuptools import setup, find_packages
 import whatapi
 
-with open('README.txt') as f:
+with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE.txt') as f:
+with open('LICENSE') as f:
     license = f.read()
 
 setup(
     name='whatapi',
-    version='0.0.1',
+    version=whatapi.__version__,
     description='What.cd API',
     long_description=readme,
     author='Isaac Zafuta',
     author_email='isaac@zafuta.com',
     url='https://github.com/isaaczafuta/whatapi',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_data = {
+        '': ['*.txt']
+    },
+    zip_safe=True
 )
